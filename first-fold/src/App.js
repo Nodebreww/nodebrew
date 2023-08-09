3ad3e63fba9c9e6dd028a21e0c4119dc215e7027
Vanshul Choudhary <cvanshul09@gmail.com>
2023-08-09 17:15:45 +0530

    First-fold done

---
+++ first-fold/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FirstFold from './components/FirstFold';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <FirstFold/>
+    </>
+  );
+}
+
+export default App;
---
+++ first-fold/src/components/FirstFold.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function FirstFold() {
+  
+
+  return (
+      <div className="section">
+      <div className="wrapper">
+        <div className="header">
+          <h1 className="h1">Stanbic CSA</h1>
+          <h3 className="h3 text-center">
+            Welcome to the OKay Inc.
+            <br />
+            Customer Service App
+          </h3>
+          <div className="downloads">
+            <a href="/" className="button-icon button w-inline-block">
+              <div className="text-block">Register</div>
+            </a>
+          </div>
+          <img src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb266bbb40333_Group%2020.svg" alt="" className="headimg"/>
+          <img src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb22902b40339_Group%2028.svg" alt="" className="headimg _2"/>
+        </div>
+      </div>
+    </div>
+  );
+}
