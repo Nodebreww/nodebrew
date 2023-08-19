981fd20c512c809f0bc2c41bdd8a1b91b743a5c3
harshikagoyal14 <goyalharshika266@gmail.com>
2023-08-19 22:01:39 +0530

    Animation done

--- first-fold/src/components/FirstFold.js
+++ first-fold/src/components/FirstFold.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "../styles.css";
+import { setupCursorAnimation } from "./cursorAnimation";
+
 
 export default function FirstFold() {
   
+   useEffect(() => {
+    setupCursorAnimation();
+  }, []);
+
 
   return (
-      <div className="section">
+    <div className="section">
+
       <div className="wrapper">
         <div className="header">
           <h1 className="h1">Stanbic CSA</h1>
@@ -18,9 +26,30 @@ export default function FirstFold() {
               <div className="text-block">Register</div>
             </a>
           </div>
-          <img src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb266bbb40333_Group%2020.svg" alt="" className="headimg"/>
-          <img src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb22902b40339_Group%2028.svg" alt="" className="headimg _2"/>
-        </div>
+    
+      <img
+        src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb266bbb40333_Group%2020.svg"
+        alt=""
+        className="headimg"
+        id="img-1"
+       
+      />
+     <img
+     src="https://assets.website-files.com/5e8b5e18380eb29314b40327/5e8b5e18380eb22902b40339_Group%2028.svg"
+     alt=""
+     style={{
+      position: "absolute",
+      bottom: "0",
+      right: "0",
+    
+     }}
+     className="headimg_2"
+     id="img-2"
+      />
+
+
+      </div>
+
       </div>
     </div>
   );
