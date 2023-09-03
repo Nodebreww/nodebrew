8bea92455fcdae407a09d75b46eb33e84ccf2810
Sathwika <gavva.1@iitj.ac.in>
2023-09-03 18:06:30 +0530

    Details animations

--- first-fold/src/App.js
+++ first-fold/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import FirstFold from './components/FirstFold';
 import Navbar from './components/Navbar';
+import Details from './components/Details';
 import Sponsors from './components/Sponsors';
+import FeedbackForm from './components/FeedbackForm';
 
 function App() {
   return (
     <>
       <Navbar/>
       <FirstFold/>
+      <Details/>
       <Sponsors/>
+      <FeedbackForm />
     </>
   );
 }
